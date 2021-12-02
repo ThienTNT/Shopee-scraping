c7e215eec10b7aafa9206bfa91685ff318f4724a
TNT43 <thienboybt@gmail.com>
2021-12-02 15:01:47 +0700

    Create shopee's template function

--- main.py
+++ main.py
@@ -3,4 +3,19 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 import time
 
+
+def get_url(search_term):
+    template = 'https://shopee.vn/search?keyword={}'
+    search_term = search_term.replace(' ', '%20')
+
+    # Add term query to url
+    url = template.format(search_term)
+
+    # Add page query placeholder
+    url += '&page={}'
+
+    return url
+
+
+
 print("Code done!!!")
